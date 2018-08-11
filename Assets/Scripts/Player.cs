ba1240e034fb1b1dca5c45998b734fa9a3d2b61d
Lui Rocha <luifr@usp.br>
2018-08-11 15:58:33 -0300

    botoes para o celular

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,9 @@ using UnityEngine.Networking;
 
 public class Player : NetworkBehaviour {
 
+	Joystick joystick;
+	Button button;
+
 	[SyncVar]
 	public string username;
 	public float speed;
@@ -24,6 +27,9 @@ public class Player : NetworkBehaviour {
 		rb2d = GetComponent<Rigidbody2D>();
 		SpriteRenderer spr = GetComponent<SpriteRenderer>();
 		if(spr != null) spr.color = Color.red;
+
+		joystick = GameObject.Find("Joystick").GetComponent<Joystick>();
+		button = GameObject.Find("Button").GetComponent<Button>();
 	}
 
 	[ClientRpc]
@@ -60,21 +66,41 @@ public class Player : NetworkBehaviour {
 
 	}
 
-	private void Update() {
-		if(!isLocalPlayer)
-			return;
+	// private void Update() {
+	// 	if(!isLocalPlayer)
+	// 		return;
 		
 
-		//Logica de movimento
-		float verticalAxis = Input.GetAxis("Vertical");
-		float horizontalAxis = Input.GetAxis("Horizontal");
-		Debug.Log("Vertical: " + verticalAxis + " Horizontal: " + horizontalAxis);
-		if(Mathf.Abs(verticalAxis) > 0 || Mathf.Abs(horizontalAxis) > 0) {
-			float angle = Mathf.Atan2(verticalAxis,horizontalAxis);
-			Vector3 direction = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0);
-			transform.position += speed * Time.deltaTime * direction;
-			transform.rotation = Quaternion.Euler(0,0,angle * Mathf.Rad2Deg);
-		}
-	}
+	// 	//Logica de movimento
+	// 	float verticalAxis = Input.GetAxis("Vertical");
+	// 	float horizontalAxis = Input.GetAxis("Horizontal");
+	// 	Debug.Log("Vertical: " + verticalAxis + " Horizontal: " + horizontalAxis);
+	// 	if(Mathf.Abs(verticalAxis) > 0 || Mathf.Abs(horizontalAxis) > 0) {
+	// 		float angle = Mathf.Atan2(verticalAxis,horizontalAxis);
+	// 		Vector3 direction = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0);
+	// 		transform.position += speed * Time.deltaTime * direction;
+	// 		transform.rotation = Quaternion.Euler(0,0,angle * Mathf.Rad2Deg);
+	// 	}
+	// }
 
+
+	private void Update(){
+
+		if(!isLocalPlayer){
+			return;
+		}
+
+		Vector3 moveVector = (Vector3.right * joystick.Horizontal + Vector3.up * joystick.Vertical);
+
+		if (moveVector != Vector3.zero){ 
+			transform.rotation = Quaternion.LookRotation(Vector3.forward, moveVector);
+			transform.Translate(moveVector * speed * Time.deltaTime, Space.World);
+		}
+
+		if(button.Pressed){
+			Debug.Log("tiro");
+		}
+
+	}
 }
+
