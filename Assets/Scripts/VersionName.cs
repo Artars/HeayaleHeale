f7b2be6cdab257423cbe098b9c5190b3b766299e
Arthur Souza <arsbr.souza@gmail.com>
2019-09-05 21:42:55 -0300

    Fixed version and added version text

---
+++ Assets/Scripts/VersionName.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VersionName : MonoBehaviour
+{
+    public Text textRef;
+    public string textPreVersion = "Version ";
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(textRef != null)
+        {
+            textRef.text = textPreVersion + Application.version;
+        }
+    }
+
+}
